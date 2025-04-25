47b59dbfad953b9cf98c9e703362aa870c2d809d
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-04-25 15:39:00 -0300

    desafio 1 e 2 concluídos

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -53,6 +53,7 @@ function autenticar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var cpf = req.body.cpfServer;
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
@@ -61,7 +62,9 @@ function cadastrar(req, res) {
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
+    } else if (cpf == undefined) {
+        res.status(400).send("Seu cpf está undefined!");
+    }else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
@@ -70,7 +73,7 @@ function cadastrar(req, res) {
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(cpf, nome, email, senha, fkEmpresa)
             .then(
                 function (resultado) {
                     res.json(resultado);
