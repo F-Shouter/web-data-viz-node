0f4cf038975862d4911e1bfb8195d77fce252bc9
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-04-30 00:02:59 -0300

    reorganizando projeto com API

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,28 +1,26 @@
 var database = require("../database/config")
 
-function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-    var instrucaoSql = `
-        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(cpf, nome, email, senha, fkEmpresa) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",cpf, nome, email, senha, fkEmpresa);
+function cadastrar(apelido_jogador1, apelido_jogador2) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", apelido_jogador1, apelido_jogador2);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (cpf, nome, email, senha, fk_empresa) VALUES ('${cpf}','${nome}', '${email}', '${senha}', '${fkEmpresa}');
+        INSERT INTO usuario (apelido_jogador1, apelido_jogador2) VALUES ('${apelido_jogador1}','${apelido_jogador2}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
+function retornar() {
+    var instrucaoSql = `
+      SELECT apelido_jogador1, apelido_jogador2 FROM usuario 
+      ORDER BY id DESC LIMIT 1;`; // Busca o último registro
+    return database.executar(instrucaoSql);
+  }
+
 module.exports = {
-    autenticar,
-    cadastrar
+    cadastrar,
+    retornar
 };
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -2,14 +2,26 @@ var express = require("express");
 var router = express.Router();
 
 var usuarioController = require("../controllers/usuarioController");
+var usuarioModel = require("../models/usuarioModel")
 
 //Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
-})
+});
 
-router.post("/autenticar", function (req, res) {
-    usuarioController.autenticar(req, res);
+router.get("/retornar", function (req, res) {
+    usuarioModel.retornar() // Chama a função no model
+        .then(resultado => {
+            if (resultado.length > 0) {
+                res.json(resultado[0]); // Retorna o primeiro resultado como JSON
+            } else {
+                res.status(404).json({ erro: "Nenhum usuário encontrado" });
+            }
+        })
+        .catch(erro => {
+            console.log(`#ERRO: ${erro}`);
+            res.status(500).json({ erro: erro });
+        });
 });
 
 module.exports = router;
